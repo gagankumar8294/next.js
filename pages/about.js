205e05ae03add28de03741a184769dea5138498a
Gagan-Sky <gagankumar8294@gmail.com>
2022-12-26 22:40:22 +0530

    first-step

---
+++ pages/about.js
@@ -0,0 +1,7 @@
+const About = () => {
+    return <div>
+        This is the About page
+    </div>
+};
+
+export default About
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.container}>
       <h1>Hello everything Removed</h1>
       <p>Just for "Trial purpose"</p>
     </div>
-  )
+  );
 }
